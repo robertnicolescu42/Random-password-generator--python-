2fe47a53616bc64019da4d53ab82cb0920f285c2
Robert Nicolescu <robertnicolescu42@yahoo.com>
2019-05-06 00:08:48 +0300

    THE CODE WORKS!
    The script now works perfectly.
    I have to code the randomness of making random letters uppercase (or just one word)

--- RandomPasswordGenerator/RandomPasswordGenerator.py
+++ RandomPasswordGenerator/RandomPasswordGenerator.py
@@ -2,69 +2,64 @@
 # de 400 de cuvinte
 import random
 
-def rand_word_gen(file,lang):
+def rand_word_gen(lang):
     myline = 0
     myline2 = 0
-    cuvant1 = "none"
-    cuvant2 = "none2"
-
+    #myline1 = cuvant1
+    #myline2 = cuvant2
     if lang=="1":
-        #file2 = open("wordsENG2.txt", "r")
         file1=open("wordsENG.txt","r")
         file2=open("wordsENG2.txt","r")
     elif lang=="2":
-        #file2 = open("wordsRO2.txt", "r")
         file1=open("wordsRO.txt","r")
         file2=open("wordsRO2.txt","r")
-
     lines = file1.read().splitlines()
-    myline =random.choice(str(lines))
+    myline =random.choice(lines)
 
     lines2 = file2.read().splitlines()
 
     if rand_num_gen()%2 == 0:
-        cuvant1 = str(myline.upper())
+        myline = myline.upper()
+
     if rand_num_gen()%3 == 0:
         myline2 = myline + random.choice(lines2)
-    elif rand_num_gen()%3 == 1:
-        myline2 = random.choice(lines2) + myline
     elif rand_num_gen()%3 == 2:
         myline2 = myline
-    cuvant2 = cuvant2 + str(myline2)
-    cuvant1 = cuvant1 + cuvant2
+    else:
+        myline2 = random.choice(lines2) + myline
     return myline2
 
 def rand_symb_gen():
 	lines = open('symbols.txt').read().splitlines()
 	myline = random.choice(lines)
-	return myline
+	return str(myline)
 
 # se genereaza un numar random de maxim 4 cifre
 def rand_num_gen():
 	return random.randint(1,1000)
 
 ###########################
-def gen_word_and_number(file,lang):
-	rw=str(rand_word_gen(file,lang))
+def gen_word_and_number(lang):
+	rw=str(rand_word_gen(lang))
 	rn=str(rand_num_gen())
 	return rw,rn
 
 # functii principale #######################################
-def easy_pass_gen(file,lang):
-	randomWord, randomNumber = gen_word_and_number(file,lang)
+def easy_pass_gen(lang):
+	randomWord, randomNumber = gen_word_and_number(lang)
 	if rand_num_gen()%2 == 0:
 		easypass = randomWord + str(randomNumber)
 	else:
 		easypass = str(randomNumber) + randomWord
 	return easypass
 
-def strong_pass_gen(file,lang):
+def strong_pass_gen(lang):
     temppass = rand_symb_gen()
     for i in range(rand_num_gen()%4):
         symbol = rand_symb_gen()
         temppass = temppass + symbol
 
-    epg=easy_pass_gen(file,lang)
+    epg=easy_pass_gen(lang)
     #print(epg)
     #de aici rezulta ca problema de acum e cu easy_pass_gen, nu returneaza un cuvant for some reason
     #problema e rezolvata (?), acum mai e doar problema ca uneori se genereaza doar numere random, inca nu stiu unde. somn usor
@@ -88,17 +83,18 @@ def passcheck(passd):
         return 1
     else: return 0
 
-def genpass(answer,file,lang):
+def genpass(answer,lang):
     while True:
         if answer == "1":
-            passd=easy_pass_gen(file,lang)
+            passd=easy_pass_gen(lang)
         else:
-            passd=strong_pass_gen(file,lang)
-
+            passd=strong_pass_gen(lang)
+        return passd
         if passcheck(passd)==1:
             return passd
         else:
-            genpass(answer,file,lang)
+            print("problema cu recursion!")
+            genpass(answer,lang)
 
 #def main():
 #    file = open("wordsENG2.txt", "r")
@@ -114,15 +110,8 @@ def genpass(answer,file,lang):
 
 def main():
     lang = input("EN/RO?")
-    file1=open("wordsENG.txt","r")
-    file2=open("wordsRO.txt","r")
     answer = input("Do you want a weak or strong password ? (1=weak, 2=strong) : ")
-    if lang==1:
-        print(genpass(answer,file1,lang))
-    else:
-        print(genpass(answer,file2,lang))
-    file1.close()
-    file2.close()
+    print(genpass(answer,lang))
 
 
 if __name__== "__main__":
